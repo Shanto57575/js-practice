86a20120152d873d4d8080d9d2abb9ad57b7a93d
Shanto57575 <shanto57575@gmail.com>
2023-02-01 23:12:44 +0600

    string split

--- str.js
+++ str.js
@@ -18,3 +18,12 @@ we use string_name.startsWith("search")
 */
 console.log(str.endsWith("morning"))//true
 console.log(str.endsWith("lovely"))//false
+
+// breaking string in various way........(split)
+const str1 = "One day you will leave this world behind. So live a life you will remember";
+let first= str1.split('');//break in char...no space
+let second = str1.split('.')//break the sentence where it will find dot
+let third = str1.split('a')//giving space....break the sentence in word
+console.log(first);
+console.log(second);
+console.log(third);
