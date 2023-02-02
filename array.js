8a6656acb9117d98ee5ba8c29e5c7ebd62ad5eaf
Shanto57575 <shanto57575@gmail.com>
2023-02-02 14:55:09 +0600

    arrays

---
+++ array.js
@@ -0,0 +1,21 @@
+//Concatenate two arrays:
+let a = [1,2,3,4,5];
+let b = [6,7,8,9,10];
+let c = a.concat(b);
+console.log(c);
+
+//Concatenate multiple arrays:
+let a1 = [1,2,3,4,5];
+let b1 = [6,7,8,9,10];
+let c1 = [11,12,13,14,15];
+let d1 = [6,7,8,9,10];
+let e1 = [6,7,8,9,10];
+console.log(a1.concat(b1,c1,d1,e1));
+
+//Sorting the array
+
+let numbers = [4,1,3,2,5,6,9];
+console.log(numbers.sort());
+
+let date =  Date();
+console.log(date);
