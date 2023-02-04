368a5ad98786255c80dfee855fe8e4e249e38597
Shanto57575 <shanto57575@gmail.com>
2023-02-04 23:09:24 +0600

    rock paper scissor game

---
+++ vc.js
@@ -0,0 +1,20 @@
+let totalCost = ([
+    {
+        name: "Dano Milk",
+        price: 500
+    },
+    {
+        name: "Taaza Tea",
+        price: 200
+    },
+    {
+        name: 'Fresh Sugar',
+        price: 300
+    }
+])
+
+let sum = 0;
+for (let i = 0; i < totalCost.length; i++) {
+    sum += totalCost[i].price;
+}
+console.log(sum);
